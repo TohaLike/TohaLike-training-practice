c4229b38083459e950441c5af69b023d3b48dda3
anton <antonpaper500@gmail.comn>
2021-10-20 21:22:03 +0300

    remove extra

--- src/AccountingList/AccountingList.js
+++ src/AccountingList/AccountingList.js
@@ -15,7 +15,7 @@ import Paper from '@mui/material/Paper';
 import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown';
 import KeyboardArrowUpIcon from '@mui/icons-material/KeyboardArrowUp';
 import Card from '@mui/material/Card';
-import { flexbox } from '@mui/system';
+
 
 function createData(name, calories, fat, carbs, protein, price) {
   return {
