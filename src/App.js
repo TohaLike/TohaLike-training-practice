f97dd165617d8280ad131229cf929ff4b525d9cd
anton <antonpaper500@gmail.comn>
2021-11-22 13:04:48 +0300

    ToDo list

--- src/App.js
+++ src/App.js
@@ -2,14 +2,17 @@ import './App.css';
 import Navigation from './Navigation/Navigation';
 import AccountingList from './AccountingList/AccountingList';
 import TextField from './TextField/TextField';
+import ToDoList from './ToDo/ToDoList';
+
 
 // В <div className="App"> импортируются модули
 function App() {
   return (
     <div className="App">
-      <Navigation/>
-      <TextField/>
-      <AccountingList/>
+      <Navigation />
+      <TextField />
+      <AccountingList />
+      <ToDoList />
     </div>
   );
 }
